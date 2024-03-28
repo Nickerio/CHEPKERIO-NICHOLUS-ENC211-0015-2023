3d0e6d27fb2d2c3febd7d5cc5c44f480c26160b2
Nickerio <158273025+Nickerio@users.noreply.github.com>
2024-03-28 16:47:42 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is victor!\n");
+    printf("My course is civil engineering!\n");
+    printf("My favourite team is manchester city!\n");
+
+return 0;
+}
